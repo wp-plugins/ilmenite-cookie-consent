144514cd51624b8d910805b1123574ff37a69c27
Erik Bernskiold <Erik Bernskiold@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-23 10:16:28 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/ilmenite-cookie-consent/trunk@1204692 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/cookie-banner.js
@@ -0,0 +1,82 @@
+// Ilmenite Cookie Banner v 0.0.1
+// by Bernskiold Media
+
+// Variables
+var noCookieMode = false;						// Debug Mode: true will disable the cookie, allowing you to debug the banner.
+var consentDuration = 30;						// Duration in Days: The number of days before the cookie should expire.
+var containerID = 'cookie-consent-block';		// The ID of the notice container div
+var cookieName = 'EUCookieConsent';				// The name of the cookie
+var cookieActiveValue = '1';					// The active value of the cookie.
+
+function setComplianceCookie() {
+
+    // If no debug mode, set the cookie
+    if ( ! window.noCookieMode ) {
+
+    	// Set the consent duration into a cookie date string
+		var date = new Date();
+	    date.setTime(date.getTime()+(window.consentDuration*24*60*60*1000));
+
+	    // Set the actual cookie
+		document.cookie = window.cookieName + '=' + window.cookieActiveValue + '; expires=' + date.toGMTString() + '; path=/';
+
+	}
+
+}
+
+function createConsentDiv() {
+
+	// Get body tag
+	var bodytag = document.getElementsByTagName('body')[0];
+
+	// Create Cookie Div
+	var div = document.createElement('div');
+	div.setAttribute('id', window.containerID);
+
+	// Set the contents
+	div.innerHTML = '<p>' + ilcc.cookieConsentText + '<a class="close-cookie-block" href="javascript:void(0);" onclick="removeMe();">' + ilcc.acceptText + '</a></p>';
+
+	// Append to body
+	bodytag.appendChild(div);
+
+	// Get the height of the consent block
+	var consentBlockHeight = jQuery('#' + window.containerID).innerHeight();
+	console.log(consentBlockHeight);
+
+	// Add class to body
+	jQuery('body').addClass('has-cookie-banner');
+	jQuery('body').css('padding-top', consentBlockHeight + 'px');
+
+}
+
+function checkCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+window.onload = function(){
+    if(checkCookie(window.cookieName) != window.cookieActiveValue){
+        createConsentDiv();
+    }
+}
+
+function removeMe(){
+
+	// Hide the cookie banner
+	jQuery( '#' + window.containerID).slideToggle(function() {
+
+		// Remove cookie banner class
+		jQuery('body').removeClass('has-cookie-banner');
+		jQuery('body').css('padding-top', '0px');
+
+	});
+
+	// Set the cookie
+	setComplianceCookie();
+}
---
+++ ilmenite-cookie-consent.php
@@ -0,0 +1,167 @@
+<?php
+/*
+ *	Plugin Name: Ilmenite Cookie Consent
+ *	Plugin URI: https://github.com/bernskioldmedia/Ilmenite-Cookie-Consent
+ *	Description: A simple, developer-friendly WordPress plugin that lets visitors know that the site is using cookies.
+ *	Author: Bernskiold Media
+ *	Version: 0.2.6
+ *	Author URI: http://www.bernskioldmedia.com/
+ *	Text Domain: ilcc
+ *	Domain Path: /languages
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+class Ilmenite_Cookie_Consent {
+
+	/**
+	 * The Plugin Path
+	 * @var string
+	 */
+	public $plugin_path;
+
+	/**
+	 * The Plugin URL
+	 * @var string
+	 */
+	public $plugin_url;
+
+	/**
+	 * The Plugin Version
+	 * @var string
+	 */
+	public $plugin_version;
+
+	/**
+	* @var The single instance of the class
+	*/
+	protected static $_instance = null;
+
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	public function __construct() {
+
+		// Set Developer Mode Constant
+		if ( ! defined( 'ILCC_DEV_MODE' ) ) {
+			define( 'ILCC_DEV_MODE', false );
+		}
+
+		// Set the plugin path
+		$this->plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
+
+		// Set the plugin URL
+		$this->plugin_url = untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) );
+
+		// Set the plugin version
+		$this->plugin_version = '0.2.6';
+
+		// Add Scripts
+		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+
+		// Add Styles
+		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
+
+		// Register Settings Fields
+		add_filter( 'admin_init', array( $this, 'settings_fields' ) );
+
+		// Add Translation Loading
+		add_action( 'plugins_loaded', array( $this, 'add_textdomain' ) );
+
+	}
+
+	/**
+	 * Load the Translations
+	 */
+	public function add_textdomain() {
+		$domain = 'ilcc';
+
+		// Let users specify their own translations under WP_LANG_DIR
+		load_plugin_textdomain( $domain ) || load_plugin_textdomain( $domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Enqueue Scripts
+	 */
+	public function scripts() {
+
+		// WP Enqueue Script
+		wp_enqueue_script( 'jquery' );
+
+		// Register Scripts
+		wp_register_script( 'ilmenite-cookie-consent', $this->plugin_url . '/assets/js/cookie-banner.min.js', array( 'jquery' ), $this->plugin_version, true );
+
+		// Localize the script
+		wp_localize_script( 'ilmenite-cookie-consent', 'ilcc', array(
+			'cookieConsentText' => sprintf( apply_filters( 'ilcc_consent_text', __( '<span>This website uses cookies to enhance the browsing experience. </span>By continuing you give us permission to deploy cookies as per our <a href="%s" rel="nofollow">privacy and cookies policy</a>.', 'ilcc' ) ), get_option( 'ilcc_policy_url' ) ),
+			'acceptText'		=> apply_filters( 'ilcc_accept_text', __( 'I Understand', 'ilcc' ) ),
+		) );
+
+		// Load script if the consent cookie isn't set
+		if ( ! isset ( $_COOKIE['EUCookieConsent'] ) ) {
+			wp_enqueue_script( 'ilmenite-cookie-consent' );
+		}
+
+	}
+
+	/**
+	 * Enqueue Styles
+	 */
+	public function styles() {
+
+		// Register them...
+		wp_register_style( 'ilmenite-cookie-consent', $this->plugin_url . '/assets/css/cookie-banner.min.css', false, $this->plugin_version, 'all' );
+
+		// Enqueue if developer mode isn't turned on
+		// also don't enqueue if consent cookie is set
+		if ( false == ILCC_DEV_MODE && ! isset ( $_COOKIE['EUCookieConsent'] ) ) {
+			wp_enqueue_style( 'ilmenite-cookie-consent' );
+		}
+
+	}
+
+	/**
+	 * Admin Settings Fields
+	 */
+	public function settings_fields() {
+		$option_group = 'reading';
+
+		// Policy URL
+		register_setting( $option_group, 'ilcc_policy_url', 'esc_attr' );
+        add_settings_field( 'ilcc_policy_url', '<label for="ilcc_policy_url">' . __( 'Privacy and Cookie Policy URL' , 'ilcc' ) . '</label>' , array( $this, 'settings_fields_html' ) , $option_group );
+
+	}
+
+	function settings_fields_html() {
+        $value = get_option( 'ilcc_policy_url', '' );
+        echo '<input type="url" class="regular-text code" id="ilcc_policy_url" name="ilcc_policy_url" value="' . $value . '" />';
+        echo '<p class="description">' . __( 'Enter a link to your privacy and cookie policy where you outline the use of cookies. This link will be used in the cookie consent banner.', 'ilcc' ) . '</p>';
+    }
+
+}
+
+function IlmeniteCookieConsent() {
+    return Ilmenite_Cookie_Consent::instance();
+}
+
+// Initialize the class instance only once
+IlmeniteCookieConsent();
